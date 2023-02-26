26e9c4aa5df1e1755bb1db5b0eba11eded980665
Yofti Milkessa <yoftahemilkessa@gmail.com>
2023-02-26 17:09:43 -0500

    Modified files to this builds into a working module

---
+++ index.ts
@@ -0,0 +1,5 @@
+// Import SelectionNode and TextBuilderNode and export them.
+import SelectionNode from "./src/selectionNode";
+import TextBuilderNode from "./src/textBuilderNode";
+
+export { SelectionNode, TextBuilderNode };
--- src/index.ts
+++
@@ -1,5 +0,0 @@
-// Import SelectionNode and TextBuilderNode and export them.
-import SelectionNode from "./selectionNode";
-import TextBuilderNode from "./textBuilderNode";
-
-exports = { SelectionNode, TextBuilderNode };
